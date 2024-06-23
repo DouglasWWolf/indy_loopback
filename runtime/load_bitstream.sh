ac55245a7b9c9a18d6590984aea0ec9fc2ad7bac
DouglasWWolf <douglaswolf@yahoo.com>
2024-06-22 19:54:23 -0700

    No commit message

--- runtime/load_bitstream.sh
+++ runtime/load_bitstream.sh
@@ -1,3 +1,3 @@
 filename=$1
 test -z $filename && filename=indy_loopback.bit
-load_bitstream -hot_reset -part xcku15p_0 -pci_device 10ee:903f -vivado $VIVADO $filename
+load_bitstream -hot_reset -part xcku15p_0 -pci_device 10ee:903f -vivado "$VIVADO" $filename
